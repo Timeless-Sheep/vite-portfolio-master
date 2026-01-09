5e178b0d0435506e31eb4bdb391267d5c1346e4f
Jon Von Ivar <kharov002@gmail.com>
2026-01-09 13:02:26 +0800

    adjust text

--- src/components/Hobbies.jsx
+++ src/components/Hobbies.jsx
@@ -5,7 +5,7 @@ const Hobbies = () => {
   const hobbies = [
     {
       title: "Musician",
-      text: `I play rythm/lead guitarist in the band "Creepy Clowns" and also play electric guitar when needed. Jasfel and I have been collaborating for many years; we write original material together and perform when opportunities arise. Although we haven't released official recordings yet due to limited funds, we’re actively preparing demos and plan to publish our music as soon as possible.`,
+      text: `I play rythm/lead guitarist in the band "Creepy Clowns" and also play Bass guitar when needed. Jasfel and I have been collaborating for many years; we write original material together and perform when opportunities arise. Although we haven't released official recordings yet due to limited funds, we’re actively preparing demos and plan to publish our music as soon as possible.`,
       image:
         "https://scontent.fceb9-1.fna.fbcdn.net/v/t39.30808-6/553432257_122096534289040151_4545400960019448690_n.jpg?stp=dst-jpg_s960x960_tt6&_nc_cat=110&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeEQxUAk-P015jw10o7FoYWjDS3d8OD3YFINLd3w4PdgUrHn1RYkp0DvoyAxyG9npKby2SD63RHeIqp-ZXBgiKVP&_nc_ohc=tKWZcrTpWzUQ7kNvwGONu4c&_nc_oc=AdmLp9wruwqCpHw3fItB8YxL1m7lGdnX2it2DKzutEpTGrRFevDiWSXmfCV38Moe4Cw&_nc_zt=23&_nc_ht=scontent.fceb9-1.fna&_nc_gid=haP43qlxCrh9R57iAdT3Cg&oh=00_Afp8CFGPIde6DCeZrRK9tqEoBzwBMk2YCoTS11tfuxiQRg&oe=696645FC",
     },
